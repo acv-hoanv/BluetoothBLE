93ba42fc34d861648fbb519871bdecc703db11e4
MY-73BKCFLS129S\MyPC <hoanv@atmarkcafe.org>
2018-08-30 00:04:23 +0700

    Bluetooth BLE

---
+++ app/src/main/java/com/awesomeallapp/ninhhoa/scanbeacon/MyApplication.java
@@ -0,0 +1,56 @@
+package com.awesomeallapp.ninhhoa.scanbeacon;
+
+import android.app.Application;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+public class MyApplication extends Application implements BootstrapNotifier {
+    private static final String TAG = ".MyApplicationName";
+    private RegionBootstrap regionBootstrap;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "App started up");
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+         beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        // wake up the app when any beacon is seen (you can specify specific id filers in the parameters below)
+        Region region = new Region("com.example.myapp.boostrapRegion", null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+    }
+
+    @Override
+    public void didEnterRegion(Region region) {
+
+        Log.d("Hoa", "Da vao khu vuc");
+        Toast.makeText(this, "Da vao khu vuc", Toast.LENGTH_SHORT).show();
+        regionBootstrap.disable();
+
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+        Intent intent = new Intent(this, MainActivity.class);
+        // IMPORTANT: in the AndroidManifest.xml definition of this activity, you must set android:launchMode="singleInstance" or you will get two instances
+        // created when a user launches the activity manually and it gets launched from here.
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.startActivity(intent);
+        Log.d("Hoa", "Da roi khu vuc");
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+
+    }
+}
